262df76ff45e56578b964f7f9cc061b8ab579dda
Claire <ikim318@aucklanduni.ac.nz>
2025-02-10 09:14:06 +0900

    Modified roles with the redis

--- app/routers/roles.py
+++ app/routers/roles.py
@@ -1,13 +1,14 @@
 from datetime import datetime
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 from app.authorization import oauth2
 from app.databases import role_model, database
-from app.error import RoleException
+from app.error import RoleException, UserException
 from app.models import schemas
+from app.redis import redis_cache, redis_set
 
 router = APIRouter(prefix="/role", tags=["Role"], dependencies=[Depends(oauth2.get_api_key)])
 
@@ -23,6 +24,9 @@ def create_role(role: schemas.RoleBase, db: Session = Depends(get_db)):
         db.add(new_role)
         db.commit()
         db.refresh(new_role)
+
+        redis_set(type_="ROLE", id_=role.name, data=role_data)
+
         return new_role
     except:
         raise RoleException(errorCode=f"Role '{role.name}' already exists")
@@ -30,21 +34,38 @@ def create_role(role: schemas.RoleBase, db: Session = Depends(get_db)):
 
 @router.get("/all_roles", response_model=List[schemas.RoleBase], status_code=status.HTTP_200_OK)
 def get_roles(db: Session = Depends(get_db)):
-    return db.query(role_model.Role).all()
+    roles = db.query(role_model.Role).all()
+    for role in roles:
+        cache_key = f"ROLE:{role.name}"
+        cache_data = redis_cache.get(cache_key)
+        if not cache_data:
+            role_data = {k: v for k, v in user.__dict__.items() if not k.startswith("_")}
+            redis_set(type_="ROLE", id_=role.name, data=role_data)
+    return roles
 
 
 @router.put("/", status_code=200, response_model=schemas.RoleBase)
-def update_role(role: str, update_role: schemas.RoleBase, db: Session = Depends(get_db)):
-    role_db = db.query(role_model.Role).filter(role_model.Role.name == role).first()
+def update_role(
+    role: str,
+    update_role: schemas.RoleBase,
+    db: Session = Depends(get_db),
+    current_user: schemas.TokenData = Depends(oauth2.get_api_key),
+):
+    if current_user.role_id != 1:
+        raise UserException(statusCode=403, errorCode="Must be admin to update role")
+    try:
+        role_db = db.query(role_model.Role).filter(role_model.Role.name == role).first()
 
-    if not role_db:
-        raise HTTPException(status_code=409, detail=f"Role {role} not found")
+        if not role_db:
+            raise RoleException(status_code=409, detail=f"Role {role} not found")
 
-    for key, value in update_role.items():
-        setattr(role_db, key, value)
+        for key, value in update_role.items():
+            setattr(role_db, key, value)
 
-    role_db.updated_at = datetime.now()
-    db.commit()
-    db.refresh(role_db)
+        role_db.updated_at = datetime.now()
+        db.commit()
+        db.refresh(role_db)
 
-    return role_db
+        return role_db
+    except:
+        raise
